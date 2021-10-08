f225c7065fdf6af76f95a39433ebba05f0d63b87
EC2 Default User <ec2-user@ip-172-31-49-202.ec2.internal>
2021-10-08 10:36:01 +0000

    応用課題5

--- bookers_2/app/controllers/users_controller.rb
+++ bookers_2/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
     def index
       @users = User.all
       @book = Book.new
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
       @user= User.find(params[:id])
       @book = Book.new
       @books = @user.books
+      @users = User.all
     end
 
     def edit
--- bookers_2/config/routes.rb
+++ bookers_2/config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+
   end
+  get '/searches', to: 'searches#search'
   get "/home/about" =>"home#about"
   resources :books do
     resources :book_comments, only: [:create, :destroy]
 
     resource :favorites, only: [:create, :destroy]
-  
+
   end
   get	'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
